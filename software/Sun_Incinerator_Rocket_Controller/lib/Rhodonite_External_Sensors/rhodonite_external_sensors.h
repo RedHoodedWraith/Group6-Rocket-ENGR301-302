fe57c77359624edfd503af106434a048fd6b5399
Penny Peneueta <penny@Pennys-MacBook-Pro.local>
2020-07-29 00:47:22 +1200

    Added constructor and a few variables into the rhodonite code as a start The variables so far only cover part of the sensors, particularly the GPS ans barometer. These are subject to be being changed as the code develops.

--- software/Sun_Incinerator_Rocket_Controller/lib/Rhodonite_External_Sensors/rhodonite_external_sensors.h
+++ software/Sun_Incinerator_Rocket_Controller/lib/Rhodonite_External_Sensors/rhodonite_external_sensors.h
@@ -1,6 +1,23 @@
+/**
+ * 'Rhodonite' External Sensors Library
+ * 
+ * This library will handle the external conditions, using data retrieved from the following external sensors: 
+ * GPS, Inertial Measurement Unit (IMU), Barometer and Servo Controller. 
+ */
 #ifndef RHODONITE_EXTERNAL_SENSORS
 #define RHODONITE_EXTERNAL_SENSORS
 
 #include "lars_main.h"
+
+class Rhodonite{
+public: 
+    Rhodonite(); 
+    float air_pressure; 
+    float time;
+    float longitude;
+    float latitude;
+    float altitude; 
+private:
+}
 
 #endif
