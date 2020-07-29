1e80c5edb3b8855ed73a22b4b537cb2eab576e48
Penny Peneueta <penny@Pennys-MacBook-Pro.local>
2020-07-29 12:24:04 +1200

    Additions made to the rhodonite code that include getters and setters

--- software/Sun_Incinerator_Rocket_Controller/lib/Rhodonite_External_Sensors/rhodonite_external_sensors.h
+++ software/Sun_Incinerator_Rocket_Controller/lib/Rhodonite_External_Sensors/rhodonite_external_sensors.h
@@ -2,7 +2,7 @@
  * 'Rhodonite' External Sensors Library
  * 
  * This library will handle the external conditions, using data retrieved from the following external sensors: 
- * GPS, Inertial Measurement Unit (IMU), Barometer and Servo Controller. 
+ * GPS, Inertial Measurement Unit (IMU) and Barometer. 
  */
 #ifndef RHODONITE_EXTERNAL_SENSORS
 #define RHODONITE_EXTERNAL_SENSORS
@@ -12,12 +12,44 @@
 class Rhodonite{
 public: 
     Rhodonite(); 
+    float getAirPressure() {
+      return air_pressure;
+    }
+    float getTime() {
+      return time;
+    }
+    float getLongitude(){
+        return longitude;
+    }
+    float getLatitude(){
+        return latitude;
+    }
+    float getAltitude(){
+        return altitude;
+    }
+
+private:
     float air_pressure; 
     float time;
     float longitude;
     float latitude;
     float altitude; 
-private:
+
+    void setAirPressure(float a) {
+        air_pressure = a;
+    }
+    void setTime(float t) {
+        time = t;
+    }
+    void setLongitude(float l){
+        longitude = l;
+    }
+    void setLatitude(float l){
+        latitude = l;
+    }
+    void setAltitude(float a){
+        altitude = a;
+    }
 }
 
 #endif
